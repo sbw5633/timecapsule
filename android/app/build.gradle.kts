8d5a4956347f250a3d604768cefbe8f0b52de477
sbw5633 <sbw5633@gmail.com>
2025-11-03 21:44:16 +0900

    chore: change package/bundle id to w.app.timecapsule (Android/iOS) and move MainActivity

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -23,7 +23,7 @@ dependencies {
 }
 
 android {
-    namespace = "com.example.timecapsule"
+    namespace = "w.app.timecapsule"
     compileSdk = 36  // 35에서 36으로 변경
     ndkVersion = "27.0.12077973"
 
@@ -38,7 +38,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.timecapsule"
+        applicationId = "w.app.timecapsule"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 23
--- android/app/src/main/kotlin/w/app/timecapsule/MainActivity.kt
+++ android/app/src/main/kotlin/w/app/timecapsule/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.timecapsule
+package w.app.timecapsule
 
 import io.flutter.embedding.android.FlutterActivity
 
